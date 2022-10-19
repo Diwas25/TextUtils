b67c9b1d700faab5d3523554fbc4436a8363dd11
Diwas Kumar Arya <diwasarya25@gmail.com>
2022-10-19 06:22:09 +0530

    added word count and preview features

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -8,12 +8,19 @@ export default function Textform(props) {
         setText(newText);
     };
 
+    const handleLoClick = () => {
+      // console.log("Uppercase was clicked");
+      let newText = text.toLowerCase();
+      setText(newText);
+  };
+
     const handleOnChange = (event) => {
         // console.log("On Change");
         setText(event.target.value);
     };
   return (
-    <div>
+    <>
+    <div class="container">
       <h2>{props.heading}</h2>  
       <div className="mb-3">
         <textarea
@@ -25,7 +32,16 @@ export default function Textform(props) {
         ></textarea>
        
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+      <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+      <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to LowerCase</button>
     </div>
+    <div class="container my-3">
+      <h2>Your Text Summary</h2>
+      <p>{text.split(" ").length} words and {text.length} characters</p>
+      <p>{0.008 * text.split(" ").length} Minute Read</p>
+      <h2>Preview</h2>
+      {text}
+    </div>
+    </>
   );
 }
